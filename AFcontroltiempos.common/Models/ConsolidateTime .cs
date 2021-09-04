987a265c813b2a36d20393b46b7b1179f0732c14
milena1211 <francyzapata96372@correo.itm.edu.co>
2021-09-03 23:40:53 -0500

    model to consolidate times

---
+++ AFcontroltiempos.common/Models/ConsolidateTime .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFcontroltiempos.common.Models
+{
+    class ConsolidateTime
+    {
+        public int EmployeeId { get; set; }
+        public DateTime WorkDate { get; set; }
+        public int MinutesWorked { get; set; }
+    }
+}
