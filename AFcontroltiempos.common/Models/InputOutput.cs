d23ea700eea10211b320258c3d758f62a3fe613c
milena1211 <francyzapata96372@correo.itm.edu.co>
2021-09-03 23:32:33 -0500

    model for inputs and outputs

---
+++ AFcontroltiempos.common/Models/InputOutput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AFcontroltiempos.common.Models
+{
+    internal class InputOutput
+    {
+        public int EmployeeId { get; set; }
+        public DateTime DateInputOrOutput { get; set; }
+        public int Type { get; set; }
+        public bool IsConsolidated { get; set; }
+    }
+}
