ccadb148d227c74b3dd9a4b74b5eca03f9865d17
milena1211 <francyzapata96372@correo.itm.edu.co>
2021-09-05 10:42:29 -0500

    Entities

---
+++ AFcontroltimes.Function/Entities/ConsolidateTimeEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace AFcontroltimes.Function.Entities
+{
+    public class ConsolidateTimeEntity : TableEntity
+    {
+        public int EmployeeCode { get; set; }
+        public DateTime ConsolidatedDate { get; set; }
+        public int MinutesWorked { get; set; }
+    }
+}
---
+++ AFcontroltimes.Function/Entities/RecordTimeEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace AFcontroltimes.Function.Entities
+{
+    public class RecordTimeEntity : TableEntity
+    {
+        public int EmployeeCode { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int Type { get; set; }
+        public bool IsConsolidated { get; set; }
+    }
+}
