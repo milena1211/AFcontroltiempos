72c2d64d1dd3827ec9abbd72407e72cd9b7aff42
milena1211 <francyzapata96372@correo.itm.edu.co>
2021-09-05 10:29:00 -0500

    Models

---
+++ AFcontroltimes.Common/Models/ConsolidateTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AFcontroltimes.Common.Models
+{
+    public class ConsolidateTime
+    {
+        public int EmployeeCode { get; set; }
+        public DateTime ConsolidatedDate { get; set; }
+        public int MinutesWorked { get; set; }
+    }
+}
---
+++ AFcontroltimes.Common/Models/RecordTime.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AFcontroltimes.Common.Models
+{
+    public class RecordTime
+    {
+        public int EmployeeCode { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public int Type { get; set; }
+        public bool IsConsolidated { get; set; }
+    }
+}
